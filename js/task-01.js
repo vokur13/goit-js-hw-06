97d51fb64bb88c2c2bf46f218ecdec620d8fee7f
vokur13 <vokur13@gmail.com>
2022-08-08 18:50:12 +0300

    task-10 fixed

--- js/task-01.js
+++ js/task-01.js
@@ -1,37 +1,9 @@
-// const listItemEl = document.querySelectorAll('.item');
-// console.log('Number of categories:', listItemEl.length);
-
-// listItemEl.forEach(item => {
-//   item.querySelectorAll('h2').forEach(title => {
-//     console.log('Category:', title.textContent);
-//   });
-//   console.log('Elements:', item.querySelectorAll('li').length);
-// });
-
-//
-
 const listItemEl = document.querySelectorAll('.item');
 console.log('Number of categories:', listItemEl.length);
 
-const listItemTitleEl = listItemEl.children;
-console.log(listItemTitleEl);
-
-console.log(
-  listItemEl.forEach(item => {
-    item.firstElementChild;
-  })
-);
-
-// listItemEl.forEach(item => {
-//   item.querySelectorAll('h2').forEach(title => {
-//     console.log('Category:', title.textContent);
-//   });
-//   console.log('Elements:', item.querySelectorAll('li').length);
-// });
-
-//
-
-// const firstNavItem = navEl.firstElementChild;
-// console.log(firstNavItem);
-// console.log(navEl.children);
-// console.log(navEl.children[2]);
+listItemEl.forEach(item => {
+  item.querySelectorAll('h2').forEach(title => {
+    console.log('Category:', title.textContent);
+  });
+  console.log('Elements:', item.querySelectorAll('li').length);
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,38 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const controlsEl = document.querySelector('#controls');
+const boxesEl = document.querySelector('#boxes');
+
+const refs = {
+  inputEl: controlsEl.firstElementChild,
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+};
+
+refs.inputEl.addEventListener('input', onNumberGet);
+refs.createBtn.addEventListener('click', createBoxes);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+function onNumberGet(event) {
+  return event.currentTarget.value;
+}
+
+function createBoxes() {
+  let boxHeight = 20;
+  let boxWidth = 20;
+  for (let index = 0; index < refs.inputEl.value; index += 1) {
+    const box = document.createElement('div');
+    boxHeight += 10;
+    boxWidth += 10;
+    box.style.height = `${boxHeight}px`;
+    box.style.width = `${boxWidth}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesEl.append(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = ' ';
+}
