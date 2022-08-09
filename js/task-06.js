5caf1ff68b751067a14da11d903e582ea0d6dfa5
vokur13 <vokur13@gmail.com>
2022-08-09 18:33:02 +0300

    task-06 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,10 @@
 const textInput = document.querySelector('#validation-input');
 
-const checkInputLength = event => {
-  event.currentTarget.value.length < textInput.dataset.length
-    ? textInput.classList.add('invalid')
-    : textInput.classList.add('valid');
-};
-
 textInput.addEventListener('blur', checkInputLength);
+
+function checkInputLength(event) {
+  console.log(event.currentTarget.value.length);
+  event.currentTarget.value.length === Number(textInput.dataset.length)
+    ? textInput.classList.add('valid')
+    : textInput.classList.add('invalid');
+}
