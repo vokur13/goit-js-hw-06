965563c9397f65080a329099d0b12364e06cd9c0
vokur13 <vokur13@gmail.com>
2022-08-10 14:37:07 +0300

    task-06 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,15 @@
 const textInput = document.querySelector('#validation-input');
 
+textInput.addEventListener('focus', onInputClassRemove);
 textInput.addEventListener('blur', checkInputLength);
 
 function checkInputLength(event) {
-  console.log(event.currentTarget.value.length);
   event.currentTarget.value.length === Number(textInput.dataset.length)
     ? textInput.classList.add('valid')
     : textInput.classList.add('invalid');
+}
+
+function onInputClassRemove() {
+  textInput.classList.remove('valid');
+  textInput.classList.remove('invalid');
 }
